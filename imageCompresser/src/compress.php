19c600cd5b1ba78f58556aa3cbcce1c0017b4f91
sawers014 <74411308+sawers014@users.noreply.github.com>
2024-07-06 22:24:06 +0200

    the download page now works properly and you can UPLOAD your image

--- imageCompresser/src/compress.php
+++ imageCompresser/src/compress.php
@@ -1,4 +1,10 @@
 <?php
-$video=$_POST[''];
+$video=$_POST['myfile'];
+$destination=null;
+
+#imagejpeg($video, $destination, 75); #reduce image with amazing built in function
 
 ?>
+<html>
+<a href="\asdf.jpg" download="image">Link text</a>
+</html>
